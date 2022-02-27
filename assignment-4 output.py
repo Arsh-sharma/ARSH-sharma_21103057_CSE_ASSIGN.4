3f5bfe6518ca26cb94099e323779a170b2cecaca
Arsh-sharma <97118125+Arsh-sharma@users.noreply.github.com>
2022-02-27 13:41:25 +0530

    Add files via upload

---
+++ assignment-4 output.py
@@ -0,0 +1,45 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\ARSH SHARMA\Documents\assi.py ===============
+MOVE DISK 1 FROM ROD A TO ROD C
+MOVE DISK 2 FROM ROD A TO ROD B
+MOVE DISK 1 FROM ROD C TO ROD B
+MOVE DISK 3 FROM ROD A TO ROD C
+MOVE DISK 1 FROM ROD B TO ROD A
+MOVE DISK 2 FROM ROD B TO ROD C
+MOVE DISK 1 FROM ROD A TO ROD C
+Enter number of rows: 6
+USING ITERATIVE PROCEDURE
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+USING RECURSSIONS
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+Enter first integer: 7
+Enter second integer: 2
+False
+False
+QUOTIENT IS NOT EQUAL TO ZERO
+REMAINDER IS NOT EQUAL TO ZERO
+REQUIRED LIST IS: [7.5, 5, 8.5, 6, 9.5, 7]
+SET DATATYPE {5, 6, 7.5, 8.5, 9.5, 7}
+IMMUTABLE SET IS:  frozenset({7, 5, 6, 7.5, 8.5, 9.5})
+MAXIMUM VALUE AND ITS HASH VALUE IS: 1152921504606846985
+OBJECT CREATED!
+STUDENT'S NAME IS ARSH SHARMA AND ROLL NUMBER IS 21103057.
+DONE
+SALARY OF MEHAK INCREASED TO 70000
+SALARY UPDATED!
+Record of VIREN deleted!
+Enter any word: rose
+Enter a new according to game rules: why
+The letters do not match, FAKE FRIENSHIP!!
